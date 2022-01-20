7bce4a6d16ca8706b64c8407638ba72422c615b3
Sven Ponelat <svenponelat@hotmail.com>
2022-01-20 12:12:41 -0400

    raw

---
+++ src/lib.rs
@@ -0,0 +1,76 @@
+/*
+    My box drawing library
+
+    2022.01.20      csp
+
+
+*/
+
+
+pub mod normal;
+
+/// This module contains thick borders
+pub mod thick;
+
+/// This module contains double borders
+pub mod double;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/thick.rs
@@ -0,0 +1,38 @@
+
+
+
+
+/// Horizontal
+pub const H:&str="━";
+
+/// Vertical
+pub const V:&str="┃";
+
+/// Upper Left corner
+pub const UL:&str="┏";
+
+/// Upper Right corner
+pub const UR:&str="┓";
+
+/// Lower Left corner
+pub const LL:&str="┗";
+
+/// Lower Right corner
+pub const LR:&str="┛";
+
+/// T symbol facing right
+pub const TR:&str="┣";
+
+/// T symbol facing left
+pub const TL:&str="┫";
+
+/// T symbol facing down
+pub const TD:&str="┳";
+
+/// T symbol facing up
+pub const TU:&str="┻";
+
+/// Cross symbol
+pub const CR:&str="╋";
+
+
